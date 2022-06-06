5985fa96f768dbef0b873c5290e92cd72b83f5ec
Salvador <2054888970310@ingenieria.usac.edu.gt>
2022-06-06 10:47:28 -0600

    Se creo el endpoint para la resta
    Endpoint: /resta
    Puerto: 3000
    Resta de dos valores enteros

--- API2/server2.js
+++ API2/server2.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = 3000;
 const morgan = require('morgan');
 
 app.use(morgan('combined'));
@@ -15,6 +15,16 @@ app.get('/', (req, res)=>{
 
 app.get('/info', (req, res)=>{
     res.send('Salvador de Jesus Lopez Bautista - 201314059')
+})
+
+/********************* ENDPOINT TYPE POST *********************/
+app.post('/resta', (req, res)=>{
+    const n1 = Number(req.query.num1)
+    const n2 = Number(req.query.num2)
+    
+    const result = n1 - n2
+    console.log(result)
+    res.send(`El resultado de ${n1} - ${n2} es ${result}`)
 })
 
 /**************************************************************/
